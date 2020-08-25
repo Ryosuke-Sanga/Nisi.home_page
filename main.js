dbef983164ba6db2b1446a24577412f7deba3b48
Ryosuke-Sanga <67500160+Ryosuke-Sanga@users.noreply.github.com>
2020-08-25 15:47:14 +0900

    commit 3

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-'use strict'
+'use strict';
+// window.addEventListener('load',() => {
+
+//   setInterval(() => {
+//    let target = currentIndex + 1;
+
+//     if (target === images.length) {
+//       target = 0;
+//     }
+//     document.querySelectorAll('.carousel__thumbnails > li')[target].click();
+//   }, 1000);
+// });
 
 const images = [
   'css/images/image.000.jpg',
@@ -10,12 +21,40 @@ const images = [
   'css/images/image.006.jpg',
 ];
 
-let currentIndex = 1;
+
+let currentIndex = 0;
 
 const mainImage = document.getElementById('carousel__main');
 mainImage.src = images[currentIndex]; 
 
-for( let [index,image] of images.entries()){
+
+for( let [index, image] of images.entries()){
+
+
+  const img = document.createElement('img');
+  img.src = image;
+
+  const li = document.createElement('li');
+  if (index === currentIndex) {
+    li.classList.add('current');
+  }
+
+  li.addEventListener('click', () => {
+    mainImage.src = image;
+    mainImage.classList.add('active');
+
+    setTimeout(() => {
+      mainImage.classList.remove('active');
+    }, 800);
+    
+    const thumbnails = document.querySelectorAll('.carousel__thumbnails > li');
+    thumbnails[currentIndex].classList.remove('current');
+    currentIndex = index;
+    thumbnails[currentIndex].classList.add('current');
+  });
+
+  li.appendChild(img);
+  document.querySelector('.carousel__thumbnails').appendChild(li);
 }
 
 const next = document.getElementById('carousel__next');
@@ -24,6 +63,7 @@ next.addEventListener('click', () => {
   if (target === images.length) {
     target = 0;
   }
+  document.querySelectorAll('.carousel__thumbnails > li')[target].click();
 });
 
 const prev = document.getElementById('carousel__prev');
@@ -32,4 +72,5 @@ prev.addEventListener('click', () => {
   if (target < 0) {
     target = images.length - 1;
   }
+  document.querySelectorAll('.carousel__thumbnails > li')[target].click();
 });
