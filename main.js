408e86b156d298591c11d8678417a603f2937def
Ryosuke-Sanga <67500160+Ryosuke-Sanga@users.noreply.github.com>
2020-08-26 22:24:12 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,76 +1,46 @@
 'use strict';
-// window.addEventListener('load',() => {
 
-//   setInterval(() => {
-//    let target = currentIndex + 1;
-
-//     if (target === images.length) {
-//       target = 0;
-//     }
-//     document.querySelectorAll('.carousel__thumbnails > li')[target].click();
-//   }, 1000);
-// });
-
-const images = [
-  'css/images/image.000.jpg',
-  'css/images/image.001.jpg',
-  'css/images/image.002.jpg',
-  'css/images/image.003.jpg',
-  'css/images/image.004.jpg',
-  'css/images/image.005.jpg',
-  'css/images/image.006.jpg',
+const pics_src = [
+  "pics/0.jpg",
+  "pics/1.jpg",
+  "pics/2.jpg",
+  "pics/3.jpg",
+  "pics/4.jpg",
+  "pics/5.jpg",
+  "pics/6.jpg",
 ];
-
-
-let currentIndex = 0;
-
-const mainImage = document.getElementById('carousel__main');
-mainImage.src = images[currentIndex]; 
-
-
-for( let [index, image] of images.entries()){
-
-
-  const img = document.createElement('img');
-  img.src = image;
-
-  const li = document.createElement('li');
-  if (index === currentIndex) {
-    li.classList.add('current');
+let num = -1;
+function slideshow_timer(){
+  if (num === 6){
+    num = 0;
+  } 
+  else {
+    num ++;
   }
+  document.getElementById("mypic").src = pics_src[num];
+}
+ 
+setInterval(slideshow_timer, 2000);
 
-  li.addEventListener('click', () => {
-    mainImage.src = image;
-    mainImage.classList.add('active');
 
-    setTimeout(() => {
-      mainImage.classList.remove('active');
-    }, 800);
-    
-    const thumbnails = document.querySelectorAll('.carousel__thumbnails > li');
-    thumbnails[currentIndex].classList.remove('current');
-    currentIndex = index;
-    thumbnails[currentIndex].classList.add('current');
-  });
 
-  li.appendChild(img);
-  document.querySelector('.carousel__thumbnails').appendChild(li);
+
+function goBack(){
+  if (num === 0) {
+      num = 6;
+  }
+  else {
+      num --;
+  }
+  document.getElementById("mypic").src = pics_src[num];
 }
 
-const next = document.getElementById('carousel__next');
-next.addEventListener('click', () => {
-  let target = currentIndex + 1;
-  if (target === images.length) {
-    target = 0;
+function goForward(){
+  if (num === 6) {
+      num = 0;
   }
-  document.querySelectorAll('.carousel__thumbnails > li')[target].click();
-});
-
-const prev = document.getElementById('carousel__prev');
-prev.addEventListener('click', () => {
-  let target = currentIndex - 1;
-  if (target < 0) {
-    target = images.length - 1;
+  else {
+      num ++;
   }
-  document.querySelectorAll('.carousel__thumbnails > li')[target].click();
-});
+  document.getElementById("mypic").src = pics_src[num];
+}
